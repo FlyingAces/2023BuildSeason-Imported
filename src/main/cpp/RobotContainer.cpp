60e930e49b076a658e6f1c5ec9c1c7c072cc1476
Giulio Alessi <giulio.alessi@acecharterhigh.org>
2023-03-02 21:30:33 -0800

    Deleted all bindings

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -32,8 +32,6 @@ RobotContainer::RobotContainer() {
 
 void RobotContainer::ConfigureButtonBindings() {
   // Configure your button bindings here
-  m_DriverYButton.WhileTrue(&m_PIDLevel);
-  m_DriverAButton.WhileTrue(new DriveStraightPID(&m_DriveTrain, 1_m));
 }
 
 frc2::Command* RobotContainer::GetAutonomousCommand() {
