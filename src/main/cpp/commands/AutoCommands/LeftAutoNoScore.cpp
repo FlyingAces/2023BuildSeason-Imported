1dfea4af85b047a1a8e3994fc7931cd004b4e033
Giulio Alessi <giulio.alessi@acecharterhigh.org>
2023-03-02 20:50:09 -0800

    Added three auto modes. Needs testing, measurements and working PID drive/turn

---
+++ src/main/cpp/commands/AutoCommands/LeftAutoNoScore.cpp
@@ -0,0 +1,10 @@
+#include "commands/AutoCommands/LeftAutoNoScore.h"
+
+LeftAutoNoScore::LeftAutoNoScore(DriveTrainSubsystem* p_DriveTrain, GyroSubsystem* p_Gyro) : mp_DriveTrain{p_DriveTrain}, mp_Gyro{p_Gyro} {
+    AddCommands(DriveStraightPID(mp_DriveTrain, 1_m),
+    TurnToAnglePID(mp_DriveTrain, mp_Gyro, 90_deg),
+    DriveStraightPID(mp_DriveTrain, 1_m),
+    TurnToAnglePID(mp_DriveTrain, mp_Gyro, 90_deg),
+    DriveStraightPID(mp_DriveTrain, 1_m),
+    PIDLevel(mp_DriveTrain, mp_Gyro));
+}
---
+++ src/main/cpp/commands/AutoCommands/RightAutoNoScore.cpp
@@ -0,0 +1,10 @@
+#include "commands/AutoCommands/RightAutoNoScore.h"
+
+RightAutoNoScore::RightAutoNoScore(DriveTrainSubsystem* p_DriveTrain, GyroSubsystem* p_Gyro) : mp_DriveTrain{p_DriveTrain}, mp_Gyro{p_Gyro} {
+    AddCommands(DriveStraightPID(mp_DriveTrain, 1_m),
+    TurnToAnglePID(mp_DriveTrain, mp_Gyro, -90_deg),
+    DriveStraightPID(mp_DriveTrain, 1_m),
+    TurnToAnglePID(mp_DriveTrain, mp_Gyro, -90_deg),
+    DriveStraightPID(mp_DriveTrain, 1_m),
+    PIDLevel(mp_DriveTrain, mp_Gyro));
+}
